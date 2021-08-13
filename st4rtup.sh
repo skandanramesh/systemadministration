5a2a81213709e722328bb20d001ae27dcc60387a
Skandan Ramesh <54405114+skandyman@users.noreply.github.com>
2021-08-14 01:20:43 +0530

    Create st4rtup.sh

---
+++ st4rtup.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash
+
+cp future.txt /usr/local/bin/future.txt
+cp attendance.log /usr/local/bin/attendance.log
+chmod u+x genUser.sh
+chmod u+x permit.sh
+chmod u+x schedule1.sh
+chmod u+x schedule2.sh
+chmod u+x attendance.sh
