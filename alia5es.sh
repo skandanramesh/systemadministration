3af87c29b7c458518af13e9eebc8b164d0aaf32d
Skandan Ramesh <54405114+skandyman@users.noreply.github.com>
2021-08-14 01:19:52 +0530

    Create alia5es.sh

---
+++ alia5es.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+
+echo "alias genUser='cp -p genUser.sh /usr/local/bin/genUser.sh';" >> ~/.bashrc
+echo "alias permit='cp -p permit.sh /usr/local/bin/genUser.sh';" >> ~/.bashrc
+echo "alias schedule1='cp -p schedule1.sh /usr/local/bin/schedule1.sh';" >> ~/.bashrc
+echo "alias schedule2='cp -p schedule2.sh /usr/local/bin/schedule2.sh';" >> ~/.bashrc
+echo "alias attendance='cp -p attendance.sh /usr/local/bin/attendance.sh;" >> ~/.bashrc
+source ~/.bashrc
