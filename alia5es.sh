43b7e8812fbdfad6c7cb5acb28124178090bbab7
Skandan <54405114+skandanramesh@users.noreply.github.com>
2023-10-11 12:25:30 +0530

    Create alia5es.sh

---
+++ alia5es.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash
+
+echo "alias genUser='./genUser.sh';" >> ~/.bashrc
+echo "alias permit='./permit.sh';" >> ~/.bashrc
+echo "alias schedule='./schedule.sh';" >> ~/.bashrc
+echo "alias s3tcron='./s3tcron.sh';" >> ~/.bashrc
+echo "alias attendance='./attendance.sh';" >> ~/.bashrc
+echo "alias genMom='./genMom.sh';" >> ~/.bashrc
+source ~/.bashrc
