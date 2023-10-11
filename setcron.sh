532328ccd8d8a5637a963c333401e028841a25bc
Skandan <54405114+skandanramesh@users.noreply.github.com>
2023-10-11 12:20:38 +0530

    Update setcron.sh

--- setcron.sh
+++ setcron.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/bash
 
-touch /etc/cron.daily/sample
-cp sample.sh /etc/cron.daily/sample
-echo " " >> /etc/cron.daily/sample
+cp future.txt /usr/local/bin/future.txt
+sed -i 's|./future.txt|/usr/local/bin/future.txt|g' schedule.sh
+touch /etc/cron.daily/meeting_update
+cp schedule.sh /etc/cron.daily/meeting_update
+echo " " >> meeting_update
+chmod u+x meeting_update
